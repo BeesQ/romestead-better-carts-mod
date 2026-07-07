3e16846055ed6f60e3b14a10cbaaf901c250483a
Jakub BeesQ Łotysz <jakublotysz@gmail.com>
2026-07-07 06:46:04 +0200

    build: target net8.0-windows and opt into game preview APIs

--- Plugin.cs
+++ Plugin.cs
@@ -1,18 +1,19 @@
+using System.Runtime.Versioning;
 using BepInEx;
 using BepInEx.NET.Common;
 using HarmonyLib;
 
+[assembly: RequiresPreviewFeatures]
+
 namespace BetterCarts;
 
 [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-public class BetterCartsPlugin : BasePlugin
-{
+public class BetterCartsPlugin : BasePlugin {
     public const string PluginGuid = "com.beesq.romestead.bettercarts";
     public const string PluginName = "Better Carts";
     public const string PluginVersion = "1.0.0";
 
-    public override void Load()
-    {
+    public override void Load() {
         ModConfig.Init(Config);
         new Harmony(PluginGuid).PatchAll();
         Log.LogInfo(PluginName + " " + PluginVersion + " loaded.");
